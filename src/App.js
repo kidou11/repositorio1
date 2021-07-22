2347c0da06f1ffc0bcc7822ac1280cc7d3e4ee9f
kidou11 <kidou9522@gmail.com>
2021-07-22 16:02:55 -0500

    Primera version de desarrollo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import EjemploDeEstado from "./components/C04estado";
+
+function App(){
+  return(
+    <div>
+      <hr/>
+      <EjemploDeEstado />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/C04estado.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const EjemploDeEstado = () => {
+    return ( 
+        <div style={{backgroundColor: 'yellowgreen'}}>
+            <h4>Componente 4. Ejemplo de definicion de estado</h4>
+        </div>
+     );
+}
+ 
+export default EjemploDeEstado;
